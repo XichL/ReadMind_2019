5c569e35b1a102715370b7796cf0aa3c890116b1
XichL <may319620@gmail.com>
2019-11-19 18:59:04 +0800

    Add files via upload
    First version

---
+++ 2019MindRead/2019MindRead.cpp
@@ -0,0 +1,10 @@
+#pragma once
+#include "CommonFunction.h"
+
+using namespace System;
+
+int main(array<System::String ^> ^args)
+{
+    return 0;
+	
+}
---
+++ 2019MindRead/stdafx.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+// TODO: 在此參考您的程式所需的其他標頭
+#include <windows.h> 
+#include <WinBase.h>
+#include <iostream>
+#pragma comment(lib, "user32.lib")
