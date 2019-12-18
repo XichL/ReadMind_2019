db93026e78cbbad4a65dca0d2f9eebc7862ff4eb
XichL <may319620@gmail.com>
2019-12-19 02:23:11 +0900

    Update image and ascii
    新增ID與圖片

--- MindRead2019/MindRead2019.cpp
+++ MindRead2019/MindRead2019.cpp
@@ -12,12 +12,51 @@ int main(array<System::String ^> ^args)
 	FunctionSet^ MRFun = gcnew FunctionSet();
 	//----------------------------------------------------
 #if 1
-	String^ TestString = "1Tu6k4mLULyGMOOA";
+	//String^ TestString = "1Tu6k4mLULyGMOOA";
+	String^ TestString = "PZxOYX6P7tpCNE12";
+
 
 	array<Byte>^ out;
 	String^ dst;
 	String^ backString;
 	MRFun->TransTo8bitASCII(TestString, dst);
+
+	String^ newStr;
+	for (int i = 0; i < dst->Length / 4; i++)
+	{
+		std::string c1, c2, c3, c4;
+		c1 = dst[4 * i + 2];
+		c2 = dst[4 * i + 3];
+		c3 = dst[4 * i + 0];
+		c4 = dst[4 * i + 1];
+
+		String^ s1 = gcnew String(c1.c_str());
+		String^ s2 = gcnew String(c2.c_str());
+		String^ s3 = gcnew String(c3.c_str());
+		String^ s4 = gcnew String(c4.c_str());
+
+		newStr += s1 + s2 + s3 + s4;
+	}
+
+	//back
+	String^ newStr2;
+	for (int i = 0; i < newStr->Length / 4; i++)
+	{
+		std::string c1, c2, c3, c4;
+		c1 = newStr[4 * i + 2];
+		c2 = newStr[4 * i + 3];
+		c3 = newStr[4 * i + 0];
+		c4 = newStr[4 * i + 1];
+
+		String^ s1 = gcnew String(c1.c_str());
+		String^ s2 = gcnew String(c2.c_str());
+		String^ s3 = gcnew String(c3.c_str());
+		String^ s4 = gcnew String(c4.c_str());
+
+		newStr2 += s1 + s2 + s3 + s4;
+	}
+	MRFun->ASCII8bitToLetter(newStr2, backString);
+
 	MRFun->ASCII8bitToLetter(dst, backString);
 	//MRFun->TransTo8bitASCII(TestString, out);
 	//MRFun->SaveData(out, out->Length, 1, 1, "Test.txt");
